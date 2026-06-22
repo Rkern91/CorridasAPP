55cade9eb261f5c282448f93bb55f6ed78c7ae23
Rkern91 <156999@upf.br>
2026-06-22 00:34:20 -0300

    Adiciona index.php na raiz para redirecionar à tela inicial em View/

---
+++ index.php
@@ -0,0 +1,5 @@
+<?php
+  // Ponto de entrada da aplicação. Todas as Views ficam em View/;
+  // este arquivo apenas redireciona a raiz para a tela inicial.
+  header("Location: View/index.php");
+  exit;
